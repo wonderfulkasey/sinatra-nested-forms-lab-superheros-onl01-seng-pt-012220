c8fe57236a77d61ace9164e54b65f99aaf1970be
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-07 04:10:47 +0000

    Done.

--- spec/super_sinatra_forms_spec.rb
+++ spec/super_sinatra_forms_spec.rb
@@ -1,7 +1,7 @@
 describe App do
 
   describe 'GET /' do
-    before do 
+    before do
       get '/'
     end
 
@@ -9,20 +9,20 @@ describe App do
       expect(last_response.status).to eq(200)
     end
 
-    it 'renders super hero form' do 
+    it 'renders super hero form' do
       expect(last_response.body).to include("Create a Team and Heroes!")
       expect(last_response.body).to include("<form")
     end
   end
 
-  describe 'POST /teams' do
-    it 'submits the form' do
-      visit '/'
-      fill_in("team[name]", :with => "Team Ruby")
+describe 'POST /teams' do
+it 'submits the form' do
+visit '/'
+   fill_in("team[name]", :with => "Team Ruby")
       fill_in("team[motto]", :with => "We love Ruby!")
       fill_in("member1_name", :with => "Amanda")
-      fill_in("member1_power", :with => "Ruby")
-      fill_in("member1_bio", :with => "I love Ruby!")
+     fill_in("member1_power", :with => "Ruby")
+           fill_in("member1_bio", :with => "I love Ruby!")
       fill_in("member2_name", :with => "Arel")
       fill_in("member2_power", :with => "JavaScript")
       fill_in("member2_bio", :with => "I love JavaScript!")
@@ -36,13 +36,13 @@ describe App do
       expect(page).to have_text("Hero Name: Amanda")
       expect(page).to have_text("Hero Power: Ruby")
       expect(page).to have_text("Hero Biography: I love Ruby!")
-      expect(page).to have_text("Hero Name: Arel")
+     expect(page).to have_text("Hero Name: Arel")
       expect(page).to have_text("Hero Power: JavaScript")
       expect(page).to have_text("Hero Biography: I love JavaScript!")
       expect(page).to have_text("Hero Name: Katie")
       expect(page).to have_text("Hero Power: Sinatra")
-      expect(page).to have_text("Hero Biography: I love Sinatra!")
-    end
+     expect(page).to have_text("Hero Biography: I love Sinatra!")
+   end
   end
-  
+
 end
